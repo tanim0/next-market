54b5cdf1e2a39900b82b8f8d091a3b56a43a9ec7
tanim0 <nyokki0731@gmail.com>
2024-04-26 18:18:40 +0900

    create.js async await 追加
    DBとの接続→書き込みの順に実行

--- pages/api/item/create.js
+++ pages/api/item/create.js
@@ -1,11 +1,11 @@
 import connectDB from "../../utils/database"
 import { ItemModel } from "../../utils/schemaModels"
 
-const createItem = (req, res) => {
+const createItem = async(req, res) => {
   try{
-    connectDB()
-    console.log(req.body.title)
-    ItemModel.create(req.body) // ItemModelを使ってMongoDBに書き込み
+    await connectDB()
+    console.log(req.body)
+    await ItemModel.create(req.body) // ItemModelを使ってMongoDBに書き込み
     return res.status(200).json({message: "アイテム作成成功"})
   } catch(err) {
     return res.status(400).json({message: "アイテム作成失敗"})
