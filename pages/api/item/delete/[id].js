0321c7e4d724b97436f2691eae80c00c8f25a7ce
tanim0 <nyokki0731@gmail.com>
2024-04-26 22:04:32 +0900

    アイテムデータ削除機能(delete)

---
+++ pages/api/item/delete/[id].js
@@ -0,0 +1,15 @@
+import connectDB from "../../../utils/database";
+import { ItemModel } from "../../../utils/schemaModels";
+
+// データの柵書にはItemModelに格納されたdeleteOne()を使う
+const deleteItem = async(req, res) => {
+  try{
+    await connectDB()
+    await ItemModel.deleteOne({_id: req.query.id})
+    return res.status(200).json({message: "アイテム削除成功"})
+  }catch(err){
+    return res.status(400).json({message: "アイテム削除失敗"})
+  }
+}
+
+export default deleteItem
