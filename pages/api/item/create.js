003bcc356fd4ab3ad5a675ae4a32d53d162361e0
tanim0 <nyokki0731@gmail.com>
2024-04-26 18:28:06 +0900

    fomをSchemaの形式に修正

--- pages/api/item/create.js
+++ pages/api/item/create.js
@@ -4,7 +4,6 @@ import { ItemModel } from "../../utils/schemaModels"
 const createItem = async(req, res) => {
   try{
     await connectDB()
-    console.log(req.body)
     await ItemModel.create(req.body) // ItemModelを使ってMongoDBに書き込み
     return res.status(200).json({message: "アイテム作成成功"})
   } catch(err) {
