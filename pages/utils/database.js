c690d7368ae15372e88ef754f18f9e9e9a5509e4
tanim0 <nyokki0731@gmail.com>
2024-04-26 17:59:56 +0900

    MongoDBパスワード置き換えミス修正

--- pages/utils/database.js
+++ pages/utils/database.js
@@ -5,9 +5,9 @@ import mongoose from "mongoose";
 const connectDB = async() => {
   try {
     // mongodb.net/と?の間に「appDataBase」とデータベース名書いている
-    await mongoose.connect("mongodb+srv://tanim0:<ZAr35IvkYTzdUeVS>@tanim0.yyqyc1e.mongodb.net/appDataBase?retryWrites=true&w=majority&appName=tanim0")
+    await mongoose.connect("mongodb+srv://tanim0:ZAr35IvkYTzdUeVS@tanim0.yyqyc1e.mongodb.net/appDataBase?retryWrites=true&w=majority&appName=tanim0")
     console.log("Success: Connected to MangoDB")
-  } catch(err) {
+  }catch(err){
     console.log("Failure: Unnconnected to MangoDB")
     throw new Error() // エラーが発生したことを知らせる
   }
