48de6e69aa43d8d6dc99952b0578a839788f4281
tanim0 <nyokki0731@gmail.com>
2024-04-26 17:43:33 +0900

    async awaitでmongoose.connect()処理後にconsoleを実行

--- pages/utils/database.js
+++ pages/utils/database.js
@@ -2,10 +2,10 @@
 // JSとMongoDBの接続ではmongooseというパッケージよく使われる(Shema,Model)
 import mongoose from "mongoose";
 
-const connectDB = () => {
+const connectDB = async() => {
   try {
     // mongodb.net/と?の間に「appDataBase」とデータベース名書いている
-    mongoose.connect("mongodb+srv://tanim0:<ZAr35IvkYTzdUeVS>@tanim0.yyqyc1e.mongodb.net/appDataBase?retryWrites=true&w=majority&appName=tanim0")
+    await mongoose.connect("mongodb+srv://tanim0:<ZAr35IvkYTzdUeVS>@tanim0.yyqyc1e.mongodb.net/appDataBase?retryWrites=true&w=majority&appName=tanim0")
     console.log("Success: Connected to MangoDB")
   } catch(err) {
     console.log("Failure: Unnconnected to MangoDB")
