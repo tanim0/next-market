31154a42b53a307cff4c8550f9ddb93df865cb52
tanim0 <nyokki0731@gmail.com>
2024-04-26 21:58:39 +0900

    アイテムデータ修正(update)

---
+++ pages/api/item/update/[id].js
@@ -0,0 +1,17 @@
+import connectDB from "../../../utils/database"
+import { ItemModel } from "../../../utils/schemaModels"
+
+// データの修正にはItemModelに格納されたupdateOne()を使う
+const updateItem = async(req, res) => {
+  try{
+    await connectDB()
+    // db.collection.updateOne(filter, update, options)
+    await ItemModel.updateOne({_id: req.query.id}, req.body)
+    return res.status(200).json({message: "アイテム編集成功"})
+  }catch(err){
+    return res.status(400).json({message: "アイテム編集失敗"})
+  }
+  
+}
+
+export default updateItem
