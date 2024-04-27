31741f19573815b8265b25af709c54aecf58fa61
tanim0 <nyokki0731@gmail.com>
2024-04-27 14:43:01 +0900

    ログイン状態維持機能実装(Json Web Token)

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -1,10 +1,17 @@
-// ログイン機能は
-// その人が登録を済ませているかチェック(DBからユーザーデータを取得しその人がすでに登録しているか調べる)
-// 登録を済ませている場合はpasswordが正しいかチェック
-
+import jwt from "jsonwebtoken"
 import connectDB from "../../utils/database"
 import { UserModel } from "../../utils/schemaModels"
 
+// ログインを維持する仕組みとして今回はトークン方式を使う(セッション方式もある)
+// jsonwebtokenはトークンを発行するjwt.sign()と
+// ログイン後のリクエスト時にトークンの有効性を検証するjwt.verify()をセットで使う
+
+// jwt.sign(ペイロード(トークンに含ませたいデータ。ユーザー名やメールアドレスなどを入れる),シークレットキー,有効期限)
+const secret_key = "nextmarket" // シークレットキー設定
+
+// ログイン機能は
+// その人が登録を済ませているかチェック(DBからユーザーデータを取得しその人がすでに登録しているか調べる)
+// 登録を済ませている場合はpasswordが正しいかチェック
 const loguinUser = async(req, res) => {
   try{
     await connectDB()
@@ -14,6 +21,14 @@ const loguinUser = async(req, res) => {
       // ユーザーデータが存在する場合の処理
       if(req.body.password === savedUserData.password) {
         // パスワードが正しい場合の処理
+
+        // ログイン成功した場合にペイロードにメールアドレスを入れトークン発行
+        const payload = {
+          email: req.body.email,
+        }
+        const token = jwt.sign(payload, secret_key, {expiresIn: "23h"})
+        // tokenはLocalStrageに保存される
+
         return res.status(200).json({"message": "ログイン成功"})
       } else {
         return res.status(400).json({"message": "ログイン失敗(パスワードが間違っています)"})
