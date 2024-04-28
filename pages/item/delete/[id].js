890b591bc157b7111cac9ca3b87a91a7425bcf29
tanim0 <nyokki0731@gmail.com>
2024-04-28 17:31:42 +0900

    商品削除ページ作成

---
+++ pages/item/delete/[id].js
@@ -0,0 +1,60 @@
+// 商品削除ページ・・・アイテムデータを一つ読み取り、投稿の処理
+// 削除＝>バックエンドで/api/item/delete/[id].jsで処理が始まる
+// データを投稿しないPOSTリクエスト
+
+// 商品修正ページからデータのstateの保持や入力データの送信を省いたものに近い
+
+import Image from "next/image"
+
+const DeleteItem = (props) => {
+
+  const handleSubmit = async(e) => {
+    e.preventDefault() //formをbuttonで実行した際のブラウザのリロードを止める
+    try{
+      // データ送付が完了した時にバックエンドから返されるレスポンスと確認できるようresponseに格納し返されたデータをjson形式に変更
+      const response = await fetch(`http://localhost:3000/api/item/delete/${props.singleItem._id}`, {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          // 商品登録はログインが有効なユーザーのみ行えるのでトークンのデータをheadersに含めて送信
+          "authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+        // deleteは送るべきデータは何もないのでbodyは不要
+      })
+      // バックエンドで設定したレスポンス内のmessageをalertで表示
+      const jsonData = await response.json()
+      alert(jsonData.message)
+    }catch(err){
+      alert("アイテム削除に失敗しました")
+    }
+  }
+
+  return (
+    <div>
+      <h1>アイテム削除</h1>
+      <form onSubmit={handleSubmit} method="POST">
+        <h2>{props.singleItem.title}</h2>
+        <Image src={props.singleItem.image} width="750" height="500" />
+        <h3>¥{props.singleItem.price}</h3>
+        <p>{props.singleItem.description}</p>
+        <button type="submit">削除</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default DeleteItem
+
+// バックエンドからデータ取得(1つだけアイテムデータを読み取りデータをpropsへ渡す)
+// urlのデータはcontextのqueryのidに入っている
+export const getServerSideProps = async(context) => {
+  const response = await fetch(`http://localhost:3000/api/item/${context.query.id}`)
+  const singleItem = await response.json()
+
+  // データはreturnにセットすることでconst getServerSideProps = () => {...}にpropsとして渡される
+  return {
+    props: singleItem
+  }
+}
