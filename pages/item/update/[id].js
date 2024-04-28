6db965663b9f101303cd2c28b9c31c04b8ce184c
tanim0 <nyokki0731@gmail.com>
2024-04-28 16:22:25 +0900

    商品編集ページ作成

---
+++ pages/item/update/[id].js
@@ -0,0 +1,66 @@
+// 商品編集ページ・・・1つだけアイテムデータを読み取り＋アイテムデータを作成
+
+import { useState } from "react"
+
+const UpdateItem = (props) => {
+  // 初期データは取得したデータが表示されるよう
+  const [title, setTitle] = useState(props.singleItem.title)
+  const [price, setPrice] = useState(props.singleItem.price)
+  const [image, setImage] = useState(props.singleItem.image)
+  const [description, setDescription] = useState(props.singleItem.description)
+
+  const handleSubmit = async(e) => {
+    e.preventDefault() //formをbuttonで実行した際のブラウザのリロードを止める
+    try{
+      // データ送付が完了した時にバックエンドから返されるレスポンスと確認できるようresponseに格納し返されたデータをjson形式に変更
+      const response = await fetch(`http://localhost:3000/api/item/update/${props.singleItem._id}`, {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          // 商品登録はログインが有効なユーザーのみ行えるのでトークンのデータをheadersに含めて送信
+          "authorization": `Bearer ${localStorage.getItem("token")}`
+        },
+        body: JSON.stringify({
+          title: title,
+          price: price,
+          image: image,
+          description: description
+        })
+      })
+      // バックエンドで設定したレスポンス内のmessageをalertで表示
+      const jsonData = await response.json()
+      alert(jsonData.message)
+    }catch(err){
+      alert("アイテム編集に失敗しました")
+    }
+  }
+
+  return (
+    <div>
+      <h1>アイテム編集</h1>
+      <form onSubmit={handleSubmit} method="POST">
+        タイトル：<input value={title} onChange={(e) => setTitle(e.target.value)}type="text" name="title" placeholder="アイテム名" required/>
+        価格：<input value={price} onChange={(e) => setPrice(e.target.value)} type="text" name="price" placeholder="価格" required />
+        イメージ：<input value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required />
+        説明：<textarea value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" placeholder="商品説明" required></textarea>
+        <button type="submit">編集</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default UpdateItem
+
+// バックエンドからデータ取得(1つだけアイテムデータを読み取りデータをpropsへ渡す)
+// urlのデータはcontextのqueryのidに入っている
+export const getServerSideProps = async(context) => {
+  const response = await fetch(`http://localhost:3000/api/item/${context.query.id}`)
+  const singleItem = await response.json()
+
+  // データはreturnにセットすることでconst getServerSideProps = () => {...}にpropsとして渡される
+  return {
+    props: singleItem
+  }
+}
