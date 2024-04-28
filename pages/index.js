42ffb32ec284f8ac12df0486b64c8c016af93ce5
tanim0 <nyokki0731@gmail.com>
2024-04-28 14:25:45 +0900

    商品一覧ページ作成(すべてのアイテムを読み取る)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
-const ReadAllItems = () => {
+import Link from "next/link"
+import Image from "next/image"
+
+const ReadAllItems = (props) => {
   return(
     <div>
-      <h1>こんにちは</h1>
+      <div>
+        {props.allItems.map(item =>
+          <Link rel="stylesheet" href="" key={item._id}>
+            <Image src={item.image} width="750" height="500" alt="item-image"/>
+            <div>
+              <h2>¥{item.price}</h2>
+              <h3>{item.title}</h3>
+              <p>{item.description.substring(0, 80)}</p>
+            </div>
+          </Link>
+        )}
+      </div>
     </div>
   )
 }
 
 export default ReadAllItems
+
+// getServerSideProps・・・Neext.jsが用意しているでデータ取得のための特別な機能
+export const getServerSideProps = async() => {
+  const response = await fetch("http://localhost:3000/api/item/readall")
+  const allItems = await response.json()
+
+  return {
+    props: allItems
+  }
+}
