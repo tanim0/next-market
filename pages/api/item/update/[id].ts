76c3ffae9af21bfcc88bd443d0c34dd576e55196
tanim0 <nyokki0731@gmail.com>
2024-04-30 00:05:08 +0900

    api/item/update/[id].js TS化②(導入)

--- pages/api/item/update/[id].ts
+++ pages/api/item/update/[id].ts
@@ -1,15 +1,20 @@
+import type { NextApiResponse } from "next"
+import { ExtendedNextApiRequestItem, SavedItemDataType, ResMessageType} from "../../../../utils/types"
 import connectDB from "../../../../utils/database"
 import { ItemModel } from "../../../../utils/schemaModels"
 import auth from "../../../../utils/auth"
 
-// データの修正にはItemModelに格納されたupdateOne()を使う
-const updateItem = async(req, res) => {
+// アイテムデータの修正はトークンの確認が間に入るのでExtendedNextApiRequestAuth定義を使用
+const updateItem = async(req: ExtendedNextApiRequestItem, res: NextApiResponse<ResMessageType>) => {
   try{
     await connectDB()
     // 編集しようとしているアイテムデータをsingleItemに格納
-    const singleItem = await ItemModel.findById(req.query.id)
+    const singleItem: SavedItemDataType | null = await ItemModel.findById(req.query.id)
+    // | nullに対応
+    if(!singleItem) return res.status(400).json({message: "アイテムが存在していないため編集失敗"})
     // sincleItemに入っているemail(商品登録時入力)とreq.bodyに入っているemailを比較し、同一の場合だけ修正処理を実行
     if(singleItem.email === req.body.email) {
+      // データの修正にはItemModelに格納されたupdateOne()を使う
       // db.collection.updateOne(filter, update, options)
       await ItemModel.updateOne({_id: req.query.id}, req.body)
       return res.status(200).json({message: "アイテム編集成功"})
