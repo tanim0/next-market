22b18ebc85d1ca0988582d70897fd9c220b5b638
tanim0 <nyokki0731@gmail.com>
2024-04-30 16:14:39 +0900

    _app TS化②

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+import type { AppProps } from 'next/app'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='container'>
       <Header />
