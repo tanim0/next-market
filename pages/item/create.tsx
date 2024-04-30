695c57f72ea36a13e272cb36b409c11ae74274a4
tanim0 <nyokki0731@gmail.com>
2024-04-30 15:29:43 +0900

    pages/item/create TS②(コード)

--- pages/item/create.tsx
+++ pages/item/create.tsx
@@ -1,15 +1,16 @@
+import { NextPage } from "next"
 import { useState } from "react"
 import useAuth from "../../utils/useAuth"
 import Head from "next/head"
 import ImgInput from "../../components/imgInput"
 
-const CreateItem = () => {
+const CreateItem: NextPage = () => {
   const [title, setTitle] = useState("")
   const [price, setPrice] = useState("")
   const [image, setImage] = useState("")
   const [description, setDescription] = useState("")
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault() //formをbuttonで実行した際のブラウザのリロードを止める
     try{
       // データ送付が完了した時にバックエンドから返されるレスポンスと確認できるようresponseに格納し返されたデータをjson形式に変更
@@ -57,6 +58,8 @@ const CreateItem = () => {
         </form>
       </div>
     )
+  } else {
+    return <h1>ログインしてください</h1>
   }
 }
 
